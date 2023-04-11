1b6db6d3f473c5c4b587df85251787e682995665
hyh <810839700@qq.com>
2023-04-11 21:24:45 +0800

    chore: 格式化代码

--- src/hooks/useModel.ts
+++ src/hooks/useModel.ts
@@ -1,20 +1,18 @@
-import {useState} from "react";
-export const useModel = (val:any) => {
-    const [state,setState] = useState(val);
-    const newVal = new Proxy(val,{
-        get(target, key, receiver) {
-            return Reflect.get(target,key,receiver)
-        },
-        set(target, key, newValue, receiver) {
-            setState({
-                ...target,
-                [key]:newValue
-            })
-            return true
-            
-        },
-    })
-    
+import { useState } from "react";
+export const useModel = (val: any) => {
+  const [state, setState] = useState(val);
+  const newVal = new Proxy(val, {
+    get(target, key, receiver) {
+      return Reflect.get(target, key, receiver);
+    },
+    set(target, key, newValue, receiver) {
+      setState({
+        ...target,
+        [key]: newValue,
+      });
+      return true;
+    },
+  });
 
-    return [state,newVal]
-}
+  return [state, newVal];
+};
